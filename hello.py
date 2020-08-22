68d3033fe86fbd2d9a9417ebf76451fbff0fd8e5
volynvlad <volynvlad@gmail.com>
2020-08-22 17:32:12 +0300

    Add form, add post requests, add upload file

--- hello.py
+++ hello.py
@@ -1,14 +1,39 @@
-from flask import Flask
-import numpy as np
+from flask import Flask, request, abort, redirect, url_for, render_template, send_file, flash
 
-app = Flask(__name__)
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+from werkzeug import secure_filename
+
+import numpy as np
+import pandas as pd
 
 import joblib
-knn = joblib.load('knn.pkl')
+import os
+
+UPLOAD_FOLDER = './'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    file = FileField('file', validators=[DataRequired()])
+
+
+print('Loading model ...')
+model = joblib.load('model.pkl')
 
 @app.route('/')
 def hello_world():
-    print(3 + 30)
     return '<h1>Hello my friend!</h1>'
 
 
@@ -23,10 +48,131 @@ def avg(nums):
     nums = [float(num) for num in nums.split(',')]
     return f'average is {np.mean(nums)}'
 
+
+def predict_result(param):
+    is_pattern_right = True
+    wrong_symbol = None
+
+    for c in param:
+        if c is not ',' and not c.isdigit():
+            is_pattern_right = False
+            wrong_symbol = c
+            break
+        
+    if not is_pattern_right:
+        raise f"Wrong pattern! Expect nums and ','.\t\tFind {wrong_symbol}"
+    if param[0] == ',':
+        raise "Wrong pattern! Should start with num"
+    if param[-1] == ',':
+        raise "Wrong pattern! Should end with num"
+
+    param = np.array([float(num) for num in param.split(',')]).reshape(1, -1)
+    if len(*param) != 4: 
+        raise "Expect 4 numbers!"
+
+    print('Predicting ...')
+    return model.predict(param)
+
+
 @app.route('/iris/<param>')
 def iris(param):
-    param = np.array([float(num) for num in param.split(',')]).reshape(1, -1)
-    assert len(*param) == 4
+    
+    try:
+        predict = predict_result(param)
+    except:
+        return redirect(url_for('bad_request'))
 
-    return f"input - {param}, output - {knn.predict(param)}"
-    # return str(knn.predict(iris_X_test))
+    results = {
+        0: "setosa", 
+        1: "virginica",
+        2: "versicolor"
+    }
+
+    return f'<img src="/static/{results[predict[0]]}.jpg" alt="{predict[0]}">'
+
+
+@app.route('/bad_request')
+def bad_request():
+    return abort(400)
+
+
+@app.route('/iris_post', methods=['POST'])
+def add_message():
+    content = request.get_json()
+
+    try:
+        predict_ = {"class": str(predict_result(content['flower'])[0])}
+    except:
+        return redirect(url_for('bad_request'))
+    
+    return (predict_)
+
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+
+        f = form.file.data
+        print(f)
+        filename = form.name.data + '.csv'
+        
+        print('Reading ...')
+        df = pd.read_csv(f, header=None)
+        
+        print('Predicting ...')
+        predict_ = model.predict(df)
+
+        result = pd.DataFrame(predict_)
+        result.to_csv(filename, index=False, header=None)
+
+        return send_file(filename,
+                    mimetype='text/csv',
+                    attachment_filename=filename,
+                    as_attachment=True)
+
+    return render_template('submit.html', form=form)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            result_name = 'result.csv'
+            filename = secure_filename(file.filename)
+            
+            print('Reading ...')
+            df = pd.read_csv(file, header=None)
+            
+            print('Predicting ...')
+            predict_ = model.predict(df)
+
+            result = pd.DataFrame(predict_)
+            result.to_csv(result_name, index=False, header=None)
+
+            return send_file(result_name,
+                    mimetype='text/csv',
+                    attachment_filename=result_name,
+                    as_attachment=True)
+    
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
--- train.py
+++ train.py
@@ -1,8 +1,24 @@
 from sklearn import datasets
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+# from lightgbm import LGBMClassifier
 
+params =  {
+            'boosting_type': 'dart',
+            'max_depth' : 16,
+            'num_leaves': 24,
+            'nthread': -1,
+            'learning_rate': 0.01,
+            'subsample_freq': 1,
+            'colsample_bytree': 0.9,
+            'min_split_gain': 0.5,
+            'min_child_weight': 1,
+            'min_child_samples': 16,
+            'n_estimators': 1000,
+            'early_stopping_round': 10,
+}
 
-def train():
+def train(classifier, params):
     iris = datasets.load_iris()
     iris_X = iris.data
     iris_y = iris.target
@@ -13,14 +29,14 @@ def train():
     iris_y_train = iris_y[indices[:-10]]
     iris_X_test = iris_X[indices[-10:]]
     iris_y_test = iris_y[indices[-10:]]
-
-    from sklearn.neighbors import KNeighborsClassifier
-    knn = KNeighborsClassifier()
+    
+    model = classifier(**params)
     print('Training model ...')
-    knn.fit(iris_X_train, iris_y_train)
+    model.fit(iris_X_train, iris_y_train)
 
     import joblib
     print('Saving model ...')
-    joblib.dump(knn, 'knn.pkl')
+    joblib.dump(model, 'model.pkl')
 
-train()
+# train(LGBMClassifier, params)
+train(KNeighborsClassifier, {})
